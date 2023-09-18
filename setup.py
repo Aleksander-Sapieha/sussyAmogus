390523848ec9c12842ba912a7f9bfbe6e9365c2d
aleksander sapieha <129301816+guard-games@users.noreply.github.com>
2023-09-18 17:24:23 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+setuptools.setup(
+    name='sussyAmogus',
+    version='0.4',
+    author='guard_games',
+    author_email='sourceguy68@gmail.com',
+    description='a sus pip package',
+    packages=setuptools.find_packages(),
+    install_requiers = [],
+    keywords=['python', 'sus', 'sussy', 'susamogus', 'sussy baka', 'amogus'],
+     classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ sussyAmogus/__init__.py
@@ -0,0 +1,17 @@
+class ejected():
+
+    def __init__(self, name):
+        self.name = name
+
+    def ejectname(name):
+        print(f"{name} was ejected")
+
+    class imposter():
+        def __init__(self, name):
+            self.name = name
+
+        def yes(name):
+           print(f"{name} was an imposter")
+
+        def no(name):
+            print(f"{name} was not an imposter")
